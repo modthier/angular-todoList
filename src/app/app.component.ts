e00113650b8b34008f5a7f83fc86feeb034b94a1
modthier <modthier47387@live.com>
2022-08-15 10:24:43 +0200

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,29 @@ import { TodoItem } from "./Models/todoItem";
 })
 export class AppComponent {
   private list = new TodoList("Bob", [
-  new TodoItem("Go for run", true),
-  new TodoItem("Get flowers"),
-  new TodoItem("Collect tickets"),
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
   ]);
   get username(): string {
-  return this.list.user;
+    return this.list.user;
   }
   get itemCount(): number {
-  return this.list.items
-  .filter(item => !item.complete).length;
+    return this.list.items
+      .filter(item => !item.complete).length;
   }
- }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }else {
+      alert('You need to enter something');
+    }
+  }
+
+  showComplete: boolean = false;
+}
